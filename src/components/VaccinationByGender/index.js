c152aaafa2587300df003c13fb11def55dd5591c
Sarthak <sarthaksingh5710@gmail.com>
2024-03-13 05:57:49 +0000

    first commit

---
+++ src/components/VaccinationByGender/index.js
@@ -0,0 +1,37 @@
+// Write your code here
+import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+import './index.css'
+
+const VaccinationByGender = props => {
+  const {vaccinationByGenderDetails} = props
+
+  return (
+    <div className="vaccination-by-gender-container">
+      <h1 className="vaccination-by-gender-heading">Vaccination by gender</h1>
+      <PieChart width={1000} height={300}>
+        <Pie
+          cx="50%"
+          cy="50%"
+          data={vaccinationByGenderDetails}
+          startAngle={180}
+          endAngle={0}
+          innerRadius="40%"
+          outerRadius="80%"
+          dataKey="count"
+        >
+          <Cell name="Male" fill="#fecba6" />
+          <Cell name="Female" fill="#b3d23f" />
+          <Cell name="Others" fill="#a44c9e" />
+        </Pie>
+        <Legend
+          iconType="circle"
+          layout="horizontal"
+          verticalAlign="bottom"
+          align="center"
+        />
+      </PieChart>
+    </div>
+  )
+}
+
+export default VaccinationByGender
---
+++ src/components/VaccinationCoverage/index.js
@@ -0,0 +1,70 @@
+// Write your code here
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts'
+import './index.css'
+
+const VaccinationCoverage = props => {
+  const {vaccinationCoverageDetails} = props
+  const DataFormatter = number => {
+    if (number > 1000) {
+      return `${(number / 1000).toString()}k`
+    }
+    return number.toString()
+  }
+
+  return (
+    <div className="vaccination-by-coverage-container">
+      <h1 className="vaccination-by-coverage-heading">Vaccination Coverage</h1>
+      <BarChart
+        width={1000}
+        height={300}
+        data={vaccinationCoverageDetails}
+        margin={{
+          top: 5,
+        }}
+      >
+        <XAxis
+          dataKey="vaccineDate"
+          tick={{
+            stroke: 'gray',
+            strokeWidth: 1,
+          }}
+        />
+        <YAxis
+          tickFormatter={DataFormatter}
+          tick={{
+            stroke: 'gray',
+            strokeWidth: 0,
+          }}
+        />
+        <Legend
+          wrapperStyle={{
+            padding: 30,
+          }}
+        />
+        <Bar
+          dataKey="dose1"
+          name="Dose 1"
+          fill="#2d87bb"
+          barSize="20%"
+          radius={[5, 5, 0, 0]}
+        />
+        <Bar
+          dataKey="dose2"
+          name="Dose 2"
+          fill="#f54394"
+          barSize="20%"
+          radius={[5, 5, 0, 0]}
+        />
+      </BarChart>
+    </div>
+  )
+}
+
+export default VaccinationCoverage
